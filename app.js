e84a0cdfadf9db97cb8595782ddb4d3b7f5b94b5
RahulPraj <rahul971801@gmail.com>
2025-03-08 15:09:47 +0530

    task 9 -10

--- app.js
+++ app.js
@@ -214,7 +214,7 @@ app.delete('/product/delete/:id',async(req,res)=>{
     }
 })
 
-//task-8 -> create route to add product in cart
+//task-8 -> create route to see all product in cart
 app.get('/cart',async(req,res)=>{
     const {token} = req.headers;
     const decodedtoken = jwt.verify(token, "supersecret");
@@ -234,6 +234,109 @@ app.get('/cart',async(req,res)=>{
         cart:user.cart
     })
 })
+
+//task-9 -> create route to add product in cart
+app.post('/cart/add',async(req,res)=>{
+    const body = req.body;
+    const productArray = body.products;
+    let totalPrice = 0;
+    try{
+        for(const item of productArray){
+            const product = await Product.findById(item);
+
+            if(product){
+                totalPrice += product.price;
+            }
+        }
+
+        const {token} = req.headers;
+        const decodedtoken = jwt.verify(token, "supersecret");
+        const user = await User.findOne({email:decodedtoken.email});
+
+        if(!user){
+            res.status(404).json({message:"User not found"});
+        }
+        let cart;
+        if(user.cart){
+            cart = await Cart.findById(user.cart).populate("products");
+            const existingProductIds = cart.products.map((product)=>{
+                product._id.toString()
+            })
+
+            productArray.forEach(async(productId)=>{
+                if(!existingProductIds.includes(productId)){
+                    cart.products.push(productId);
+
+                    const product = await Product.findById(productId);
+                    totalPrice += product.price;
+                }
+            })
+            cart.total = totalPrice;
+            await cart.save();
+        }else{
+            cart = new Cart({
+              products: productArray,
+              total:totalPrice  
+            });
+            await cart.save();
+            user.cart = cart._id;
+            await user.save();
+        }
+        res.status(201).json({message:"cart updated succcesfully",
+            cart:cart
+        })
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"Inernal server Error"})
+    }
+})
+
+//task-10 ->  create route to delete product in cart
+app.delete("/cart/product/delete", async (req, res) => {
+    const { productID } = req.body;
+    const { token } = req.headers;
+  
+    try {
+      const decodedToken = jwt.verify(token, "supersecret");
+      const user = await User.findOne({ email: decodedToken.email }).populate("cart");
+  
+      if (!user) {
+        return res.status(404).json({ message: "User Not Found" });
+      }
+  
+      const cart = await Cart.findById(user.cart).populate("products");
+  
+      if (!cart) {
+        return res.status(404).json({ message: "Cart Not Found" });
+      }
+  
+      const productIndex = cart.products.findIndex(
+        (product) => product._id.toString() === productID
+      );
+  
+      if (productIndex === -1) {
+        return res.status(404).json({ message: "Product Not Found in Cart" });
+      }
+  
+      cart.products.splice(productIndex, 1);
+      cart.total = cart.products.reduce(
+        (total, product) => total + product.price,
+        0
+      );
+  
+      await cart.save();
+  
+      res.status(200).json({
+        message: "Product Removed from Cart Successfully",
+        cart: cart,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error Removing Product from Cart", error });
+    }
+  });
 
 let PORT = 8080;
 app.listen(PORT,()=>{
